68196938c3d009b50e57b0f332b95796d1fe5100
MandelaMan <nelsonomoro@gmail.com>
2025-10-23 13:01:38 +0300

    update to intro message

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (_req, res) => res.json({ message: "Welcome to the App" }));
+router.get("/", (_req, res) =>
+  res.json({ message: "Welcome to the App for testing" })
+);
 
 router.use("/users", require("./users"));
 router.use("/accounts", require("./accounts"));
