60cf310bf1135e2ef99337bff4198adc2f09f19b
MandelaMan <nelsonomoro@gmail.com>
2025-10-23 13:18:36 +0300

    test setup

--- api/routes/accounts.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const Accounts = require("../controllers/AccountsController");
-const router = express.Router();
-
-router.post("/", Accounts.create);
-router.get("/user/:userId", Accounts.listByUser);
-router.get("/:id", Accounts.get);
-
-module.exports = router;
--- api/routes/index.js
+++ api/routes/index.js
@@ -5,8 +5,8 @@ router.get("/", (_req, res) =>
   res.json({ message: "Welcome to the App for testing" })
 );
 
-router.use("/users", require("./users"));
-router.use("/accounts", require("./accounts"));
-router.use("/transactions", require("./transactions"));
+// router.use("/users", require("./users"));
+// router.use("/accounts", require("./accounts"));
+// router.use("/transactions", require("./transactions"));
 
 module.exports = router;
