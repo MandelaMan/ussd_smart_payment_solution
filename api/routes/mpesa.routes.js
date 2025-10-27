849eab1a0aabd0a596706472ce920061468abcc7
MandelaMan <nelsonomoro@gmail.com>
2025-10-27 11:24:06 +0300

    updated mpesa routes

--- api/routes/mpesa.routes.js
+++ api/routes/mpesa.routes.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const { test, mpesaCallback } = require("../controllers/mpesa.controller");
+const {
+  test,
+  mpesaCallback,
+  mpesaValidation,
+  mpesaConfirmation,
+} = require("../controllers/mpesa.controller");
 
 const router = express.Router();
 
 router.get("/", test);
 router.post("/callback", mpesaCallback);
+router.post("/validation", mpesaValidation);
+router.post("/confirmation", mpesaConfirmation);
 
 module.exports = router;
